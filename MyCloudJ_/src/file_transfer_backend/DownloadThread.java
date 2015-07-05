DFC-Incubator/ImageJIrodsConnector

commit 2dc2992dc5a25ee777a12acd22b19f749f674a72

Remove the iRODS downloadFolder method
The iRODS downloadFile method includes functionality both for
downloading both a file and a folder => we don't need the
implementation for the downloadFolder method.

MyCloudJ_/src/rods_backend/RodsOperations.java
@@ -135,39 +135,6 @@ public class RodsOperations implements CloudOperations {
 	@Override
 	public void downloadFolder(String cloudPath, String localPath)
 			throws CloudException {
-		String error;
-		IRODSFile irodsFile;
-
-		checkPaths(localPath, cloudPath);
-		irodsFile = null;
-
-		// build the path for the local folder
-		String folderName = GeneralUtility.getLastComponentFromPath(cloudPath,
-				RODS_DELIMITER);
-		localPath += (GeneralUtility.getSystemSeparator() + folderName);
-
-		// creates the directory on disk
-		boolean newFolder = new File(localPath).mkdirs();
-		if (!newFolder) {
-			error = "Local file system error while creating local folder "
-					+ localPath;
-			throw (new CloudException(error));
-		}
-
-		try {
-			irodsFile = irodsFileFactory.instanceIRODSFile(cloudPath);
-			File[] files = irodsFile.listFiles();
-			for (int i = 0; i < files.length; i++) {
-				if (files[i].isDirectory())
-					downloadFolder(files[i].getPath(), localPath);
-				else if (files[i].isFile())
-					downloadFile(files[i].getPath(), localPath);
-			}
-		} catch (JargonException e) {
-			e.printStackTrace();
-			error = "Error accesing the cloud file " + cloudPath;
-			throw (new CloudException(error.concat(e.getMessage())));
-		}
 	}
 
 	@Override

commit a96a6fdae3e7f66c0cad6438993ffd2461a8b63c

Bug: Fix Dropbox upload/download folder functionality
This commit fixes: 2dc2992dc5a25ee777a12acd22b19f749f674a72 and
c4092a3981568c530ff654bc0064643f6d1864cf.

Although we don't need the uploadFolder/downloadFolder interfaces for
iRODS we need them for Dropbox.

MyCloudJ_/src/file_transfer_backend/DownloadThread.java
@@ -36,7 +36,10 @@ public class DownloadThread implements Callable<Void> {
 			downloadType = isFileDownload ? "file" : "folder";
 
 			// start the download
-			cloudHandler.downloadFile(sourcePath, destPath);
+			if (isFileDownload)
+				cloudHandler.downloadFile(sourcePath, destPath);
+			else
+				cloudHandler.downloadFolder(sourcePath, destPath);
 			logger.writeLog("Downloading of " + sourcePath + " complete\n\n");
 
 			if (isFileDownload)

MyCloudJ_/src/file_transfer_backend/UploadThread.java
@@ -35,10 +35,14 @@ public class UploadThread implements Callable<Void> {
 			sourcePath = task.getSourcePath();
 			destPath = task.getDestinationPath();
 			File file = new File(sourcePath);
-			uploadType = file.isFile() ? "file" : "folder";
+			boolean isFileUpload = file.isFile();
+			uploadType = isFileUpload ? "file" : "folder";
 
-			// start the download
-			cloudHandler.uploadFile(sourcePath, destPath);
+			// start the upload
+			if (isFileUpload)
+				cloudHandler.uploadFile(sourcePath, destPath);
+			else
+				cloudHandler.uploadFolder(sourcePath, destPath);
 			logger.writeLog("Uploading of " + sourcePath + " complete \n\n");
 
 			if (file.isFile())

MyCloudJ_/src/rods_backend/RodsOperations.java
@@ -129,6 +129,7 @@ public class RodsOperations implements CloudOperations {
 	@Override
 	public void downloadFolder(String cloudPath, String localPath)
 			throws CloudException {
+		downloadFile(cloudPath, localPath);
 	}
 
 	@Override
@@ -166,6 +167,7 @@ public class RodsOperations implements CloudOperations {
 	@Override
 	public void uploadFolder(String localPath, String cloudPath)
 			throws CloudException {
+		uploadFile(localPath, cloudPath);
 	}
 
 	@Override