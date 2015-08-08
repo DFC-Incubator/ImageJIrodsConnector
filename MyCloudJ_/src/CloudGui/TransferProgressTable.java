DFC-Incubator/ImageJIrodsConnector

commit 6324d3fce6334d593558b97bb11f81832951c1ad

Remove the old logging mechanism
This commit stops updating the TasksWindow panel with log messages.
We don't need this mechanism anymore because the log messages can be found
in the progress table (provided by progress bars/transfer statistics details).

This commit also prepares the ground for integrating the TransferProgressTable
into the TasksWindow area.

MyCloudJ_/src/CloudGui/TransferProgressTable.java
@@ -222,7 +222,8 @@ public class TransferProgressTable {
 						// details
 						if (rows.get(transferId).isCanceled
 								|| rows.get(transferId).progress == 1) {
-
+							
+							// TODO: check if we should really use a swing worker
 							SwingWorker<Object, Object> sw = new SwingWorker<Object, Object>() {
 								@Override
 								public Object doInBackground() {

MyCloudJ_/src/CloudGui/MyCloudJ_.java
@@ -60,7 +60,7 @@ public class MyCloudJ_ implements PlugIn {
 
 	// thread used for downloading tasks
 	ExecutorOperations downloadExecutor;
-	
+
 	// thread used for downloading tasks
 	ExecutorOperations uploadExecutor;
 
@@ -82,7 +82,7 @@ public class MyCloudJ_ implements PlugIn {
 	 * for Dbx Login - irodsLoginRadioButton: draw screen for RODS login
 	 */
 	private JRadioButton dbxLoginRadioButton, irodsLoginRadioButton;
-	
+
 	// table with file transfer progress bars
 	private TransferProgressTable progressTable;
 
@@ -104,11 +104,12 @@ public class MyCloudJ_ implements PlugIn {
 
 	public void run(String arg) {
 		SwingUtilities.invokeLater(new Runnable() {
-		    public void run() {
-				System.out.println(javax.swing.SwingUtilities.isEventDispatchThread());
+			public void run() {
+				System.out.println(javax.swing.SwingUtilities
+						.isEventDispatchThread());
 				drawGUI();
 				assignActionListeners();
-		    }
+			}
 		});
 	}
 
@@ -191,7 +192,7 @@ public class MyCloudJ_ implements PlugIn {
 				new BtnDownloadRadioListener());
 		tasksWindow.getBtnStart().addActionListener(new BtnStartListener());
 	}
-	
+
 	private void buildFileSelectionTrees(String cloudHomeDirectoryPath,
 			String localHomeDirectoryPath) throws CloudException {
 
@@ -200,42 +201,45 @@ public class MyCloudJ_ implements PlugIn {
 		// build file browsing trees for local files
 		localFileTree = new LocalFileTree(localHomeDirectoryPath);
 	}
-	
-	public void initializeTransferThreads(CloudOperations cloudHandler, TasksWindow tasksWindow) {
+
+	public void initializeTransferThreads(CloudOperations cloudHandler,
+			TasksWindow tasksWindow) {
 		// draw the table with progress bars
 		progressTable = new TransferProgressTable();
 		progressTable.draw();
-		
+
 		// start the thread for downloading
-		downloadExecutor = new DownloadExecutor(cloudHandler, tasksWindow.getLogger(), progressTable.getProgressTableModel());
+		downloadExecutor = new DownloadExecutor(cloudHandler,
+				progressTable.getProgressTableModel());
 
 		// start the thread for uploading
-		uploadExecutor = new UploadExecutor(cloudHandler, cloudFileTree, tasksWindow.getLogger(), progressTable.getProgressTableModel());
-		
+		uploadExecutor = new UploadExecutor(cloudHandler, cloudFileTree,
+				progressTable.getProgressTableModel());
+
 		progressTable.setDownloadOperations(downloadExecutor);
 		progressTable.setUploadOperations(uploadExecutor);
 	}
-	
+
 	public void genericCloudDisconnect() {
 		// free resources
 		terminateTransferThreads();
 		freeCloudResources();
-		
+
 		// reset right panel components
 		disableJTreeGUI();
 		tasksWindow.reset();
-		
+
 		userIsConnected = false;
 	}
-	
+
 	public void terminateTransferThreads() {
 		// first, destroy the table with the progress bars
 		progressTable.destroy();
-		
+
 		downloadExecutor.terminateAllTransfers();
 		uploadExecutor.terminateAllTransfers();
 	}
-	
+
 	private void freeCloudResources() {
 		try {
 			cloudHandler.disconnect();
@@ -245,13 +249,13 @@ public class MyCloudJ_ implements PlugIn {
 			e1.printStackTrace();
 		}
 	}
-	
+
 	private void disableJTreeGUI() {
 		JFrame enclosingFrame = cloudFileTree.getEnclosingFrame();
 		if (enclosingFrame != null)
 			enclosingFrame.dispose();
 	}
-	
+
 	// A lot of Action Listeners designed as inside classes
 	class BtnDbxLoginRadioListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -277,7 +281,7 @@ public class MyCloudJ_ implements PlugIn {
 			tasksWindow.setTitle(title3);
 		}
 	}
-	
+
 	class BtnRodsLoginRadioListener implements ActionListener {
 		/**
 		 * ActionListener for the "Connect to iRODS" button - initialize the
@@ -317,8 +321,7 @@ public class MyCloudJ_ implements PlugIn {
 				dropboxLoginForm.prepareToConnect();
 			} catch (CloudException e4) {
 				JOptionPane.showMessageDialog(mainFrame, e4.getMessage(),
-						"MyCLoudJ - Access Error",
-						JOptionPane.ERROR_MESSAGE);
+						"MyCLoudJ - Access Error", JOptionPane.ERROR_MESSAGE);
 				e4.printStackTrace();
 			}
 		}
@@ -342,19 +345,18 @@ public class MyCloudJ_ implements PlugIn {
 
 					// display user info in a text area
 					displayUserInfo(dbxUtility);
-					
+
 					// start transfer threads
 					initializeTransferThreads(cloudHandler, tasksWindow);
-					
+
 					// prepare the file browsing trees
 					cloudHomeDirectoryPath = cloudHandler.getHomeDirectory();
 					buildFileSelectionTrees(cloudHomeDirectoryPath,
 							LOCAL_HOME_DIRECTORY_PATH);
-					
+
 					// enable the right panel
 					tasksWindow.enable();
-				}
-				else if (!userIsConnected && dbxAccessCode.equals(""))
+				} else if (!userIsConnected && dbxAccessCode.equals(""))
 					JOptionPane.showMessageDialog(mainFrame,
 							"Enter Access Code !",
 							"MyCLoudJ - Enter Access code",
@@ -366,18 +368,17 @@ public class MyCloudJ_ implements PlugIn {
 			}
 		}
 	}
-	
+
 	public void displayUserInfo(DropboxOperations dbxUtility) {
 		String userName, country, userQuota;
 		userName = dbxUtility.getUserName();
 		country = dbxUtility.getCountry();
 		userQuota = dbxUtility.getUserQuota();
 		dropboxLoginForm.setStatus("Connected as " + userName);
-		dropboxLoginForm.setUserInfo("Username: " + userName
-				+ "\nCountry: " + country + "\nQuota: " + userQuota
-				+ " GB");
+		dropboxLoginForm.setUserInfo("Username: " + userName + "\nCountry: "
+				+ country + "\nQuota: " + userQuota + " GB");
 	}
-	
+
 	class BtnDisConnectDbxListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -398,17 +399,17 @@ public class MyCloudJ_ implements PlugIn {
 			 * TESTING -temporary solution for not entering the credentials for
 			 * every run
 			 */
-					rodsUtilsObj.setUsername("rods");
-					rodsUtilsObj.setIrodsPassword("rods");
-					rodsUtilsObj.setHost("irods-dev.incf.org");
-					rodsUtilsObj.setPort(1247);
-					rodsUtilsObj.setZone("BragadiruZone");
-					rodsUtilsObj.setRes("");
+			rodsUtilsObj.setUsername("rods");
+			rodsUtilsObj.setIrodsPassword("rods");
+			rodsUtilsObj.setHost("irods-dev.incf.org");
+			rodsUtilsObj.setPort(1247);
+			rodsUtilsObj.setZone("BragadiruZone");
+			rodsUtilsObj.setRes("");
 
 			try {
-//				 checkLoginCredentials();
-//				 rodsUtilsObj.setCredentials(user, password, host, port, zone,
-//				 		 resource);
+				// checkLoginCredentials();
+				// rodsUtilsObj.setCredentials(user, password, host, port, zone,
+				// resource);
 
 				rodsUtilsObj.login();
 				userIsConnected = true;
@@ -425,7 +426,7 @@ public class MyCloudJ_ implements PlugIn {
 				return;
 			}
 			initializeTransferThreads(cloudHandler, tasksWindow);
-			
+
 			rodsLoginForm.setConnected(true);
 			tasksWindow.enable();
 		}
@@ -468,7 +469,7 @@ public class MyCloudJ_ implements PlugIn {
 								.concat(messages)));
 		}
 	}
-	
+
 	class BtnDisConnectRodsListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -496,7 +497,8 @@ public class MyCloudJ_ implements PlugIn {
 		public void actionPerformed(ActionEvent arg0) {
 			// upload file to cloud
 			if (tasksWindow.getUploadRadioButton().isSelected()) {
-				localFileTree.openSelectionGUI(false, localFileTree.getSelectedFilePath());
+				localFileTree.openSelectionGUI(false,
+						localFileTree.getSelectedFilePath());
 				tasksWindow.setSourcePath(localFileTree.getSelectedFilePath());
 			}
 			// download file from cloud
@@ -526,7 +528,8 @@ public class MyCloudJ_ implements PlugIn {
 						new BtnCancelListener());
 				// download file from cloud
 			} else if (tasksWindow.getDownloadRadioButton().isSelected()) {
-				localFileTree.openSelectionGUI(true, localFileTree.getSelectedFilePath());
+				localFileTree.openSelectionGUI(true,
+						localFileTree.getSelectedFilePath());
 				tasksWindow.setDestinationPath(localFileTree
 						.getSelectedFilePath());
 				tasksWindow.disableDestinationPath();
@@ -596,23 +599,27 @@ public class MyCloudJ_ implements PlugIn {
 			String destinationPath = tasksWindow.getDestinationPath();
 
 			if (sourcePath.equals("") || destinationPath.equals("")) {
-				tasksWindow.getLogger().writeLog("Error: Select the files/folder to upload/download\n\n");
+				JOptionPane.showMessageDialog(mainFrame,
+						"Select the files/folder to upload/download", "Error",
+						JOptionPane.ERROR_MESSAGE);
 				return;
 			}
-			
+
 			try {
 				if (tasksWindow.getUploadRadioButton().isSelected()) {
-					TransferTask task = new TransferTask(sourcePath, destinationPath);
+					TransferTask task = new TransferTask(sourcePath,
+							destinationPath);
 					uploadExecutor.addTask(task);
 				} else if (tasksWindow.getDownloadRadioButton().isSelected()) {
-						TransferTask task = new TransferTask(sourcePath, destinationPath);
-						downloadExecutor.addTask(task);
+					TransferTask task = new TransferTask(sourcePath,
+							destinationPath);
+					downloadExecutor.addTask(task);
 				}
 			} catch (FileTransferException e1) {
 				JOptionPane.showMessageDialog(mainFrame, e1.getError(),
 						"Limit reached", JOptionPane.WARNING_MESSAGE);
 				e1.printStackTrace();
 			}
-		} 
+		}
 	}
 }

MyCloudJ_/src/file_transfer/DownloadExecutor.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import CloudGui.Logger;
 import CloudGui.TransferProgressTable.UpdatableTableModel;
 import cloud_interfaces.CloudOperations;
 
@@ -13,22 +12,20 @@ public class DownloadExecutor implements ExecutorOperations {
 	private static final int MAX_DOWNLOADS_QUEUED = 10;
 	private static final int MAX_THREADS = 1;
 	private CloudOperations cloudHandler;
-	private Logger logger;
 	private ExecutorService executor;
 	List<DownloadThread> transfers;
 	private UpdatableTableModel model;
 
-	public DownloadExecutor(CloudOperations cloudHandler, Logger logger,
+	public DownloadExecutor(CloudOperations cloudHandler,
 			UpdatableTableModel model) {
 		this.cloudHandler = cloudHandler;
-		this.logger = logger;
 		executor = Executors.newFixedThreadPool(MAX_THREADS);
 		transfers = new ArrayList<DownloadThread>(MAX_DOWNLOADS_QUEUED);
 		for (int i = 0; i < MAX_DOWNLOADS_QUEUED; i++)
 			transfers.add(null);
 		this.model = model;
 	}
-	
+
 	@Override
 	public void addTask(TransferTask task) throws FileTransferException {
 		int transferId;
@@ -43,14 +40,11 @@ public class DownloadExecutor implements ExecutorOperations {
 				transferId = this.model.addTransfer(task.getSourcePath(),
 						task.getDestinationPath(), true);
 				DownloadThread downloadTask = new DownloadThread(task,
-						cloudHandler, logger, model, transferId);
+						cloudHandler, model, transferId);
 				transfers.set(i, downloadTask);
 
 				// submit to execution the new future task
 				executor.execute(downloadTask);
-				logger.writeLog("Downloading of " + task.getSourcePath()
-						+ " to " + task.getDestinationPath()
-						+ " is in progress...\n\n");
 				return;
 			}
 		}
@@ -58,15 +52,12 @@ public class DownloadExecutor implements ExecutorOperations {
 		throw (new FileTransferException(
 				"Maximum number of pending downloads reached\n"));
 	}
-	
+
 	@Override
 	public boolean terminateTransfer(int transferId) {
 		for (int i = 0; i < MAX_DOWNLOADS_QUEUED - 1; i++) {
 			DownloadThread futureTask = transfers.get(i);
 			if (futureTask.getTransferId() == transferId) {
-				logger.writeLog("Canceled download from"
-						+ futureTask.getTask().getSourcePath() + " to "
-						+ futureTask.getTask().getDestinationPath() + "!\n\n");
 				return futureTask.cancel(true);
 			}
 		}
@@ -78,9 +69,6 @@ public class DownloadExecutor implements ExecutorOperations {
 		for (int i = 0; i < transfers.size(); i++) {
 			DownloadThread futureTask = transfers.get(i);
 			if (futureTask != null && futureTask.isDone() == false) {
-				logger.writeLog("Canceled download from"
-						+ futureTask.getTask().getSourcePath() + " to "
-						+ futureTask.getTask().getDestinationPath() + "!\n\n");
 				futureTask.cancel(true);
 			}
 		}

MyCloudJ_/src/file_transfer/DownloadThread.java
@@ -8,47 +8,46 @@ import ij.io.Opener;
 
 import javax.swing.SwingWorker;
 
-import CloudGui.Logger;
 import CloudGui.TransferProgressTable.UpdatableTableModel;
 import cloud_interfaces.CloudException;
 import cloud_interfaces.CloudOperations;
 import cloud_interfaces.CloudTransferCallback;
 import cloud_interfaces.CloudTransferStatus;
 
-public class DownloadThread extends SwingWorker<Void, Void> implements CloudTransferCallback {
+public class DownloadThread extends SwingWorker<Void, Void> implements
+		CloudTransferCallback {
 	private CloudOperations cloudHandler;
-	private Logger logger;
 	private TransferTask task;
 	private UpdatableTableModel model;
 	private int transferId;
 	private String currFile;
 
 	public DownloadThread(TransferTask task, CloudOperations cloudHandler,
-			Logger logger, UpdatableTableModel model, int transferId) {
+			UpdatableTableModel model, int transferId) {
 		this.cloudHandler = cloudHandler;
-		this.logger = logger;
 		this.task = task;
 		this.model = model;
 		this.transferId = transferId;
-		
+
 		this.addPropertyChangeListener(new PropertyChange());
 	}
-	
+
 	class PropertyChange implements PropertyChangeListener {
 		@Override
 		public void propertyChange(PropertyChangeEvent evt) {
-			  if (evt.getPropertyName().equals("progress"))
-	            	model.updateTransferStatus(getTransferId(), (int) evt.getNewValue(), currFile, false);
+			if (evt.getPropertyName().equals("progress"))
+				model.updateTransferStatus(getTransferId(),
+						(int) evt.getNewValue(), currFile, false);
 		}
 	}
-	
+
 	@Override
 	public void statusCallback(CloudTransferStatus transferStatus) {
 		int fraction = transferStatus.getFraction();
 		currFile = transferStatus.getCurrFile();
 		firePropertyChange("progress", 0, fraction);
 	}
-	
+
 	@Override
 	public Void doInBackground() throws CloudException {
 		String sourcePath = "";
@@ -61,23 +60,21 @@ public class DownloadThread extends SwingWorker<Void, Void> implements CloudTran
 			destPath = task.getDestinationPath();
 			isFileDownload = cloudHandler.isFile(sourcePath);
 			downloadType = isFileDownload ? "file" : "folder";
-			
+
 			// start the download
 			if (isFileDownload)
 				cloudHandler.downloadFile(sourcePath, destPath, this);
 			else
 				cloudHandler.downloadFolder(sourcePath, destPath, this);
-			logger.writeLog("Downloading of " + sourcePath + " complete\n\n");
-
-			if (isFileDownload)
-				openFile(task);
+			
+			// TODO: open file from the progress table
+			//if (isFileDownload)
+			//	openFile(task);
 		} catch (CloudException e) {
 			e.printStackTrace();
-			logger.writeLog("Error downloading " + downloadType + " "
-					+ sourcePath + ". " + e.getCloudError() + "\n\n");
-			
+			//TODO: transfer this error to progress table statistics
 		}
-		
+
 		return null;
 	}
 
@@ -88,7 +85,7 @@ public class DownloadThread extends SwingWorker<Void, Void> implements CloudTran
 				task.getSourcePath(), "/");
 		fileName = GeneralUtility.getSystemSeparator() + fileName;
 		Opener openfile = new Opener();
-		//openfile.open(task.getDestinationPath() + fileName);
+		openfile.open(task.getDestinationPath() + fileName);
 	}
 
 	public TransferTask getTask() {

MyCloudJ_/src/file_transfer/UploadExecutor.java
@@ -14,16 +14,14 @@ public class UploadExecutor  implements ExecutorOperations {
 	private static final int MAX_UPLOADS_QUEUED = 10;
 	private static final int MAX_THREADS = 1;
 	private CloudOperations cloudHandler;
-	private Logger logger;
 	private CloudFileTree cloudFileTree;
 	private ExecutorService executor;
 	List<UploadThread> transfers;
 	private UpdatableTableModel model;
 	
 	public UploadExecutor(CloudOperations cloudHandler,
-			CloudFileTree cloudFileTree, Logger logger, UpdatableTableModel model) {
+			CloudFileTree cloudFileTree, UpdatableTableModel model) {
 		this.cloudHandler = cloudHandler;
-		this.logger = logger;
 		this.cloudFileTree = cloudFileTree;
 		executor = Executors.newFixedThreadPool(MAX_THREADS);
 		transfers = new ArrayList<UploadThread>(MAX_UPLOADS_QUEUED);
@@ -45,13 +43,11 @@ public class UploadExecutor  implements ExecutorOperations {
 				transferId = this.model.addTransfer(task.getSourcePath(),
 						task.getDestinationPath(), false);
 				UploadThread uploadTask = new UploadThread(task, cloudHandler,
-						cloudFileTree, logger, model, transferId);
+						cloudFileTree, model, transferId);
 				transfers.set(i, uploadTask);
 
 				// submit to execution the new future task
 				executor.execute(uploadTask);
-				logger.writeLog("Uploading of" + task.getSourcePath() + " to "
-						+ task.getDestinationPath() + " is in progress...\n\n");
 				return;
 			}
 		}
@@ -65,9 +61,6 @@ public class UploadExecutor  implements ExecutorOperations {
 		for (int i = 0; i < MAX_UPLOADS_QUEUED - 1; i++) {
 			UploadThread futureTask = transfers.get(i);
 			if (futureTask.getTransferId() == transferId) {
-				logger.writeLog("Canceled download from"
-						+ futureTask.getTask().getSourcePath() + " to "
-						+ futureTask.getTask().getDestinationPath() + "!\n\n");
 				return futureTask.cancel(true);
 			}
 		}
@@ -79,9 +72,6 @@ public class UploadExecutor  implements ExecutorOperations {
 		for (int i = 0; i < transfers.size(); i++) {
 			UploadThread futureTask = transfers.get(i);
 			if (futureTask != null && futureTask.isDone() == false) {
-				logger.writeLog("Canceled upload from"
-						+ futureTask.getTask().getSourcePath() + " to "
-						+ futureTask.getTask().getDestinationPath() + "!\n\n");
 				futureTask.cancel(true);
 			}
 		}

MyCloudJ_/src/file_transfer/UploadThread.java
@@ -9,7 +9,6 @@ import java.io.File;
 import javax.swing.SwingWorker;
 
 import CloudGui.CloudFileTree;
-import CloudGui.Logger;
 import CloudGui.TransferProgressTable.UpdatableTableModel;
 import cloud_interfaces.CloudException;
 import cloud_interfaces.CloudOperations;
@@ -18,7 +17,6 @@ import cloud_interfaces.CloudTransferStatus;
 
 public class UploadThread extends SwingWorker<Void, Void> implements CloudTransferCallback {
 	private CloudOperations cloudHandler;
-	private Logger logger;
 	private CloudFileTree cloudFileTree;
 	private TransferTask task;
 	private UpdatableTableModel model;
@@ -26,9 +24,8 @@ public class UploadThread extends SwingWorker<Void, Void> implements CloudTransf
 	private String currFile;
 	
 	public UploadThread(TransferTask task, CloudOperations cloudHandler,
-			CloudFileTree cloudFileTree, Logger logger, UpdatableTableModel model, int transferId) {
+			CloudFileTree cloudFileTree, UpdatableTableModel model, int transferId) {
 		this.cloudHandler = cloudHandler;
-		this.logger = logger;
 		this.cloudFileTree = cloudFileTree;
 		this.task = task;
 		this.model = model;
@@ -73,14 +70,12 @@ public class UploadThread extends SwingWorker<Void, Void> implements CloudTransf
 				cloudHandler.uploadFile(sourcePath, destPath, this);
 			else
 				cloudHandler.uploadFolder(sourcePath, destPath, this);
-			logger.writeLog("Uploading of " + sourcePath + " complete \n\n");
-
+			
+			// TODO: open file from the progress table
 			//if (file.isFile())
 				//openFile(task);
 		} catch (CloudException e) {
 			e.printStackTrace();
-			logger.writeLog("Error uploading " + uploadType + " " + sourcePath
-					+ ". " + e.getCloudError() + "\n\n");
 			return null;
 		}
 

commit 3ec5487df18b7c0128f70f64b232955a4aeeea02

Fix: Correct date format for transfer statistics
Fixes: 6324d3fce6334d593558b97bb11f81832951c1ad

MyCloudJ_/src/CloudGui/TransferProgressTable.java
@@ -272,7 +272,7 @@ public class TransferProgressTable {
 									error.add(statusField);
 
 									SimpleDateFormat dateFormat = new SimpleDateFormat(
-											"HH:mm:ss yyyy-mm-dd ");
+											"HH:mm:ss yyyy-MM-dd ");
 									error.add(new JLabel(
 											"Transfer queued date"));
 									Date queueDate = row.getQueuedDate();