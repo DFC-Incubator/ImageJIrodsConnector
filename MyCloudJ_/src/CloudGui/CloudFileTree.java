DFC-Incubator/ImageJIrodsConnector

commit 55172201d07bf9e5d66b551986bb5183544bf450

iRODS: Add delete file functionaity
A right-click popup menu is available in the cloud file browsing tree.
User has the possibility to delete a file from iRODS.

Unfortunately, Dbx Java API doesn't have yet support for delete operation.
User is informed about the lack of capabilities when he tries to execute
an operation which is not supported.

MyCloudJ_/src/CloudGui/CloudFileTree.java
@@ -1,8 +1,18 @@
 package CloudGui;
 
+import file_transfer.ExecutorOperations;
+import file_transfer.FileTransferException;
+import file_transfer.TransferTask;
+import file_transfer.TransferTaskCallback;
 import general.GeneralUtility;
 
 import java.awt.FlowLayout;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
@@ -11,9 +21,13 @@ import java.util.List;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.SwingUtilities;
 import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeExpansionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -28,7 +42,7 @@ import cloud_interfaces.CloudOperations;
 public class CloudFileTree {
 	private static final String CLOUD_DELIMITER = "/";
 	private String homeDirectoryPath;
-	
+
 	/**
 	 * @downloadTree: stores the complete metadata(path of folders) of cloud
 	 *                account. Used to display when user browses to select the
@@ -36,7 +50,7 @@ public class CloudFileTree {
 	 * 
 	 *                Note : Each node in downloadTree represents a file/folder
 	 */
-	private JTree downloadTree;
+	private JTree tree;
 	private DefaultTreeModel downloadTreeModel;
 	private DefaultMutableTreeNode downloadRoot;
 
@@ -60,15 +74,16 @@ public class CloudFileTree {
 	private JButton cancelButton;
 
 	private CloudOperations cloudHandler;
+	private ExecutorOperations deleteExecutor;
 
 	public void createEnclosingFrameDownload() {
-		createEnclosingFrame(downloadTree);
+		createEnclosingFrame(tree);
 	}
 
 	public void createEnclosingFrameUpload() {
 		createEnclosingFrame(uploadTree);
 	}
-	
+
 	public CloudFileTree(String homeDirectoryPath, CloudOperations cloudHandler)
 			throws CloudException {
 		this.cloudHandler = cloudHandler;
@@ -76,7 +91,7 @@ public class CloudFileTree {
 
 		downloadRoot = new DefaultMutableTreeNode(homeDirectoryPath);
 		downloadTreeModel = new DefaultTreeModel(downloadRoot);
-		downloadTree = new JTree(downloadTreeModel);
+		tree = new JTree(downloadTreeModel);
 
 		getDownloadTree().getSelectionModel().setSelectionMode(
 				TreeSelectionModel.SINGLE_TREE_SELECTION);
@@ -88,6 +103,10 @@ public class CloudFileTree {
 		getUploadTree().getSelectionModel().setSelectionMode(
 				TreeSelectionModel.SINGLE_TREE_SELECTION);
 		uploadTreeModel.reload(uploadRoot);
+
+		// right click popup = rename + delete
+		tree.addMouseListener(new RightClickAction(tree));
+		uploadTree.addMouseListener(new RightClickAction(uploadTree));
 	}
 
 	private void createEnclosingFrame(JTree fileTree) {
@@ -174,7 +193,7 @@ public class CloudFileTree {
 
 	public void expandDownloadTree() throws CloudException {
 		expandTree(getDownloadTree(), downloadTreeModel, false,
-				getSelectedNodePath(downloadTree));
+				getSelectedNodePath(tree));
 	}
 
 	private void expandTree(JTree tree, DefaultTreeModel treeModel,
@@ -202,14 +221,15 @@ public class CloudFileTree {
 		DefaultMutableTreeNode node;
 		TreePath currNodePath;
 		boolean currNodeisExpanded;
-		
+
 		// try to expand the upload tree
 		node = getNodeFromPath(uploadRoot, path);
-		if (node != null)  {
+		if (node != null) {
 			currNodePath = new TreePath(node.getPath());
 			currNodeisExpanded = uploadTree.isExpanded(currNodePath);
-			/* expand only if the folder/parent of the folder where 
-			 * we've done the upload is expanded.
+			/*
+			 * expand only if the folder/parent of the folder where we've done
+			 * the upload is expanded.
 			 */
 			if (currNodeisExpanded || isParentExpanded(node, uploadTree))
 				try {
@@ -219,72 +239,78 @@ public class CloudFileTree {
 					e.printStackTrace();
 				}
 		}
-		
+
 		// try to expand the download tree
 		node = getNodeFromPath(downloadRoot, path);
 		if (node != null) {
 			try {
 				currNodePath = new TreePath(node.getPath());
-				currNodeisExpanded = downloadTree.isExpanded(currNodePath);
-				/* expand only if: 
-				 * - the folder where've done the upload is expanded 
-				 * - the folder where we've done the upload contains only the 
-				 * uploaded file and the parent of the folder where we've done 
-				 * the upload is expanded
+				currNodeisExpanded = tree.isExpanded(currNodePath);
+				/*
+				 * expand only if: - the folder where've done the upload is
+				 * expanded - the folder where we've done the upload contains
+				 * only the uploaded file and the parent of the folder where
+				 * we've done the upload is expanded
 				 */
-				if (currNodeisExpanded || (isParentExpanded(node, downloadTree) &&
-					cloudHandler.listFiles(path).size() == 1))
-					expandTree(downloadTree, downloadTreeModel, false, path);
+				if (currNodeisExpanded
+						|| (isParentExpanded(node, tree) && cloudHandler
+								.listFiles(path).size() == 1))
+					expandTree(tree, downloadTreeModel, false, path);
 			} catch (CloudException e) {
 				System.out.println("Error expanding download tree!");
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	private boolean isParentExpanded(DefaultMutableTreeNode node, JTree tree) {
-		DefaultMutableTreeNode root = (DefaultMutableTreeNode)tree.getModel().getRoot();
-		DefaultMutableTreeNode parent = (DefaultMutableTreeNode)node.getParent();
+		DefaultMutableTreeNode root = (DefaultMutableTreeNode) tree.getModel()
+				.getRoot();
+		DefaultMutableTreeNode parent = (DefaultMutableTreeNode) node
+				.getParent();
 		TreePath parentTreePath;
-		
+
 		if (root.toString() == homeDirectoryPath)
 			return true;
-		
+
 		parentTreePath = new TreePath(parent.getPath());
-		if (tree.isExpanded(parentTreePath)) 
+		if (tree.isExpanded(parentTreePath))
 			return true;
-		
+
 		return false;
 	}
-	
-	private DefaultMutableTreeNode getNodeFromPath (DefaultMutableTreeNode root, String path) {
+
+	private DefaultMutableTreeNode getNodeFromPath(DefaultMutableTreeNode root,
+			String path) {
 		if (path == null || path.startsWith(homeDirectoryPath) == false)
 			return null;
-		
+
 		if (path.equals(root.toString()) == true)
 			return root;
-		
+
 		path = path.substring(homeDirectoryPath.length() + 1, path.length());
-		List<String> pathComponents = new ArrayList<String>(Arrays.asList(path.split(CLOUD_DELIMITER)));
-		
+		List<String> pathComponents = new ArrayList<String>(Arrays.asList(path
+				.split(CLOUD_DELIMITER)));
+
 		return getNodeFromPath(root, pathComponents);
 	}
-	
-	private DefaultMutableTreeNode getNodeFromPath(DefaultMutableTreeNode node, List<String> pathComponents) {
+
+	private DefaultMutableTreeNode getNodeFromPath(DefaultMutableTreeNode node,
+			List<String> pathComponents) {
 		boolean notFound = true;
-		if (pathComponents.size() == 0) 
+		if (pathComponents.size() == 0)
 			return node;
-	
+
 		String searchedNodeName = pathComponents.get(0);
 		pathComponents.remove(0);
-		
+
 		@SuppressWarnings("unchecked")
 		Enumeration<DefaultMutableTreeNode> children = node.children();
 		while (children.hasMoreElements() && notFound) {
 			DefaultMutableTreeNode currNode = children.nextElement();
 			if (currNode.toString().equals(searchedNodeName) == true) {
-					notFound = false;
-					return getNodeFromPath(currNode, pathComponents);
+				notFound = false;
+				return getNodeFromPath(currNode, pathComponents);
 			}
 		}
 		return null;
@@ -297,7 +323,7 @@ public class CloudFileTree {
 		TreePath parentPath = tree.getSelectionPath();
 		if (parentPath == null)
 			return null;
-		
+
 		componentNo = parentPath.getPathCount();
 
 		for (int i = 0; i < componentNo; i++) {
@@ -327,7 +353,7 @@ public class CloudFileTree {
 			}
 		});
 
-		downloadTree.addTreeExpansionListener(new TreeExpansionListener() {
+		tree.addTreeExpansionListener(new TreeExpansionListener() {
 			@Override
 			public void treeExpanded(TreeExpansionEvent event) {
 				getEnclosingFrame().pack();
@@ -349,7 +375,7 @@ public class CloudFileTree {
 	}
 
 	public JTree getDownloadTree() {
-		return downloadTree;
+		return tree;
 	}
 
 	public JTree getUploadTree() {
@@ -374,5 +400,130 @@ public class CloudFileTree {
 
 	public JButton getCancelButton() {
 		return cancelButton;
+	}
+
+	public void setDeleteExecutor(ExecutorOperations deleteExecutor) {
+		this.deleteExecutor = deleteExecutor;
+	}
+
+	class RightClickAction extends MouseAdapter {
+		private JTree tree;
+
+		public RightClickAction(JTree tree) {
+			this.tree = tree;
+		}
+
+		public void mousePressed(MouseEvent e) {
+			if (SwingUtilities.isRightMouseButton(e)) {
+				TreePath selectedPath = tree.getPathForLocation(e.getX(),
+						e.getY());
+				Rectangle pathBounds = tree.getUI().getPathBounds(tree,
+						selectedPath);
+				if (pathBounds != null
+						&& pathBounds.contains(e.getX(), e.getY())) {
+					// selected path is the right clicked node
+					tree.setSelectionPath(selectedPath);
+
+					// menu = rename + delete
+					JPopupMenu menu = new JPopupMenu();
+
+					// add the rename option
+					JMenuItem renameItem = new JMenuItem("Rename");
+					ActionListener renameListener = new RenameListener(tree);
+					renameItem.addActionListener(renameListener);
+					menu.add(renameItem);
+
+					// add the rename option
+					JMenuItem deleteItem = new JMenuItem("Delete");
+					ActionListener deleteListener = new DeleteListener(tree);
+					deleteItem.addActionListener(deleteListener);
+					menu.add(deleteItem);
+
+					menu.show(tree, pathBounds.x, pathBounds.y
+							+ pathBounds.height);
+				}
+			}
+
+		}
+	}
+
+	class RenameListener implements ActionListener {
+		private JTree tree;
+
+		public RenameListener(JTree tree) {
+			this.tree = tree;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (cloudHandler.getCloudCapabilities().isRenameSUpported() == false) {
+				JOptionPane.showMessageDialog(null,
+						"Operation not supported yet");
+				return;
+			}
+
+			String selectedNodePath = getSelectedNodePath(tree);
+			if (selectedNodePath == null)
+				return;
+			System.out.println("Rename Listener: " + selectedNodePath);
+		}
+	}
+
+	class DeleteListener implements ActionListener {
+		private JTree tree;
+
+		public DeleteListener(JTree tree) {
+			this.tree = tree;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String selectedNodePath = getSelectedNodePath(tree);
+			if (selectedNodePath == null)
+				return;
+
+			if (cloudHandler.getCloudCapabilities().isDeleteSupported() == false) {
+				JOptionPane.showMessageDialog(null,
+						"Operation not supported yet");
+				return;
+			}
+
+			// popup for delete confirmation
+			Object[] message = { "Are you sure you want to delete the file:\n"
+					+ selectedNodePath + "\n" };
+			int option = JOptionPane.showConfirmDialog(null, message,
+					"Delete Confirm", JOptionPane.OK_CANCEL_OPTION);
+			if (option == JOptionPane.OK_OPTION) { // delete is authorized
+				TransferTask task = new TransferTask(selectedNodePath, "");
+				task.setCallback(new DeleteTaskCallback(selectedNodePath));
+				try {
+					deleteExecutor.addTask(task);
+				} catch (FileTransferException e2) {
+					JOptionPane.showMessageDialog(null, e2.getError(), "Error",
+							JOptionPane.ERROR_MESSAGE);
+					e2.printStackTrace();
+				}
+
+			}
+		}
+	}
+
+	class DeleteTaskCallback implements TransferTaskCallback {
+		String selectedNodePath;
+		
+		public DeleteTaskCallback(String selectedNodePath) {
+			this.selectedNodePath = selectedNodePath;
+		}
+		
+		@Override
+		public void updateGUI() {
+			DefaultMutableTreeNode downloadNode = getNodeFromPath(downloadRoot, selectedNodePath);
+			if (downloadNode != null)
+				downloadTreeModel.removeNodeFromParent(downloadNode);
+			
+			DefaultMutableTreeNode uploadNode = getNodeFromPath(uploadRoot, selectedNodePath);
+			if (uploadNode != null)
+				uploadTreeModel.removeNodeFromParent(uploadNode);
+		}
 	}
 }

MyCloudJ_/src/CloudGui/MyCloudJ_.java
@@ -22,6 +22,7 @@ import cloud_interfaces.CloudException;
 import cloud_interfaces.CloudOperations;
 import rods_backend.RodsOperations;
 import dropbox_backend.DropboxOperations;
+import file_transfer.DeleteExecutor;
 import file_transfer.DownloadExecutor;
 import file_transfer.ExecutorOperations;
 import file_transfer.FileTransferException;
@@ -63,6 +64,9 @@ public class MyCloudJ_ implements PlugIn {
 
 	// thread used for downloading tasks
 	ExecutorOperations uploadExecutor;
+	
+	// thread used for delete tasks
+	ExecutorOperations deleteExecutor;
 
 	// ------------------------------------------------------------------------
 	// commun GUI fields, specific both to Dbx and iRODS
@@ -207,9 +211,13 @@ public class MyCloudJ_ implements PlugIn {
 		// start the thread for uploading
 		uploadExecutor = new UploadExecutor(cloudHandler, cloudFileTree, tasksWindow.getProgressTableModel());
 		
+		deleteExecutor = new DeleteExecutor(cloudHandler, cloudFileTree, tasksWindow.getProgressTableModel());
+		
 		// TODO: solve this ugly dependancy between tasksWindow and executors
 		tasksWindow.setDownloadExecutor(downloadExecutor);
 		tasksWindow.setUploadExecutor(uploadExecutor);
+		tasksWindow.setDeleteExecutor(deleteExecutor);
+		cloudFileTree.setDeleteExecutor(deleteExecutor);
 	}
 
 	public void genericCloudDisconnect() {
@@ -227,6 +235,7 @@ public class MyCloudJ_ implements PlugIn {
 	public void terminateTransferThreads() {
 		downloadExecutor.terminateAllTransfers();
 		uploadExecutor.terminateAllTransfers();
+		deleteExecutor.terminateAllTransfers();
 	}
 
 	private void freeCloudResources() {
@@ -335,13 +344,13 @@ public class MyCloudJ_ implements PlugIn {
 					// display user info in a text area
 					displayUserInfo(dbxUtility);
 
-					// start transfer threads
-					initializeTransferThreads(cloudHandler, tasksWindow);
-
 					// prepare the file browsing trees
 					cloudHomeDirectoryPath = cloudHandler.getHomeDirectory();
 					buildFileSelectionTrees(cloudHomeDirectoryPath,
 							LOCAL_HOME_DIRECTORY_PATH);
+					
+					// start transfer threads
+					initializeTransferThreads(cloudHandler, tasksWindow);
 
 					// enable the right panel
 					tasksWindow.enable();
@@ -402,7 +411,7 @@ public class MyCloudJ_ implements PlugIn {
 				rodsUtilsObj.login();
 				userIsConnected = true;
 				disableRodsLoginForm();
-
+				
 				cloudHomeDirectoryPath = cloudHandler.getHomeDirectory();
 				buildFileSelectionTrees(cloudHomeDirectoryPath,
 						LOCAL_HOME_DIRECTORY_PATH);

MyCloudJ_/src/CloudGui/TasksWindow.java
@@ -218,4 +218,8 @@ public class TasksWindow {
 	public void setUploadExecutor(ExecutorOperations uploadExecutor) {
 		progressTable.setUploadOperations(uploadExecutor);
 	}
+	
+	public void setDeleteExecutor(ExecutorOperations deleteExecutor) {
+		progressTable.setDeleteOperations(deleteExecutor);
+	}
 }

MyCloudJ_/src/CloudGui/TransferProgressTable.java
@@ -28,12 +28,14 @@ import javax.swing.table.TableCellRenderer;
 
 import file_transfer.ExecutorOperations;
 import file_transfer.FileTransferException;
+import file_transfer.Transfer;
 
 public class TransferProgressTable {
 	private JTable progressTable;
 	private UpdatableTableModel progressTableModel;
 	private ExecutorOperations downloadOperations;
 	private ExecutorOperations uploadOperations;
+	private ExecutorOperations deleteOperations;
 
 	public void draw() {
 		progressTable = new JTable() {
@@ -117,6 +119,10 @@ public class TransferProgressTable {
 	public void setDownloadOperations(ExecutorOperations downloadOperations) {
 		this.downloadOperations = downloadOperations;
 	}
+	
+	public void setDeleteOperations(ExecutorOperations deleteOperations) {
+		this.deleteOperations = deleteOperations;
+	}
 
 	public ExecutorOperations getUploadOperations() {
 		return uploadOperations;
@@ -140,7 +146,7 @@ public class TransferProgressTable {
 				Object value, boolean isSelected, boolean hasFocus, int row,
 				int column) {
 			int progress = 0;
-			String status, details;
+			String status = "", details;
 
 			setStringPainted(true);
 			// scale the progress to [0, 100]
@@ -150,9 +156,15 @@ public class TransferProgressTable {
 			// transfer status: Queued/Downloading/Uploading
 			if (progress == 0)
 				status = "Queued: ";
-			else
-				status = rows.get(row).isDownload ? "Downloading: "
-						: "Uploading: ";
+			else {
+				int transferType = rows.get(row).getTransferType();
+				if (transferType == Transfer.DOWNLOAD)
+					status = "Downloading: ";
+				else if (transferType == Transfer.UPLOAD)
+					status = "Uploading: ";
+				else if (transferType == Transfer.DELETE)
+					status = "Delete: ";
+			}
 
 			// details for transfer status: Canceled/Progress in %
 			if (rows.get(row).isCanceled)
@@ -218,32 +230,36 @@ public class TransferProgressTable {
 								public Object doInBackground() {
 									RowData row = rows.get(transferId);
 
-									JPanel details = new JPanel(new GridLayout(
-											6, 6));
-									details.add(new JLabel("Source Path"));
+									JPanel error = new JPanel(new GridLayout(
+											7, 7));
+									error.add(new JLabel("Source Path"));
 									JTextField sourceField = new JTextField(row
 											.getSource());
 									sourceField.setEditable(false);
-									details.setAlignmentX(Component.LEFT_ALIGNMENT);
-									details.add(sourceField);
+									error.setAlignmentX(Component.LEFT_ALIGNMENT);
+									error.add(sourceField);
 
-									details.add(new JLabel("Destination Path"));
+									error.add(new JLabel("Destination Path"));
 									JTextField destField = new JTextField(row
 											.getDestination());
 									destField.setEditable(false);
-									details.add(destField);
+									error.add(destField);
 
-									details.add(new JLabel("Status:"));
+									error.add(new JLabel("Status:"));
 									String statusText;
 									if (row.isCanceled)
 										statusText = "Canceled ";
 									else
 										statusText = "Successful ";
-
-									if (row.isDownload)
+									
+									
+									int transferType = row.getTransferType();
+									if (transferType == Transfer.DOWNLOAD)
 										statusText += "Download";
-									else
+									else if (transferType == Transfer.UPLOAD)
 										statusText += "Upload";
+									else if (transferType == Transfer.DELETE)
+										statusText += "Delete";
 
 									int currProgress = Math.round(((Float) row
 											.getProgress()) * 100f);
@@ -253,11 +269,11 @@ public class TransferProgressTable {
 									JTextField statusField = new JTextField(
 											statusText);
 									statusField.setEditable(false);
-									details.add(statusField);
+									error.add(statusField);
 
 									SimpleDateFormat dateFormat = new SimpleDateFormat(
 											"HH:mm:ss yyyy-mm-dd ");
-									details.add(new JLabel(
+									error.add(new JLabel(
 											"Transfer queued date"));
 									Date queueDate = row.getQueuedDate();
 									String queuedDateString = "N/A";
@@ -267,9 +283,9 @@ public class TransferProgressTable {
 									JTextField queuedDateTextField = new JTextField(
 											queuedDateString);
 									queuedDateTextField.setEditable(false);
-									details.add(queuedDateTextField);
+									error.add(queuedDateTextField);
 
-									details.add(new JLabel(
+									error.add(new JLabel(
 											"Transfer start date"));
 									Date startDate = row.getStartTransferDate();
 									String startDateString = "N/A";
@@ -279,9 +295,9 @@ public class TransferProgressTable {
 									JTextField startDateTextField = new JTextField(
 											startDateString);
 									startDateTextField.setEditable(false);
-									details.add(startDateTextField);
+									error.add(startDateTextField);
 
-									details.add(new JLabel("Transfer end date"));
+									error.add(new JLabel("Transfer end date"));
 									Date endDate = row.getEndTransferDate();
 									String endDateString = "N/A";
 									if (endDate != null)
@@ -290,12 +306,14 @@ public class TransferProgressTable {
 									JTextField endDateTextField = new JTextField(
 											endDateString);
 									endDateTextField.setEditable(false);
-									details.add(endDateTextField);
+									error.add(endDateTextField);
+									
+									error.add(new JLabel("Error"));
+									JTextField errorField = new JTextField(row.getError());
+									errorField.setEditable(false);
+									error.add(errorField);
 
-									// TODO
-									JPanel error = new JPanel();
-
-									final JComponent[] inputs = new JComponent[] { details };
+									final JComponent[] inputs = new JComponent[] { error };
 									JOptionPane.showMessageDialog(null, inputs,
 											"Transfer Details",
 											JOptionPane.PLAIN_MESSAGE);
@@ -304,7 +322,7 @@ public class TransferProgressTable {
 							};
 							sw.execute();
 							// user clicks the "Cancel" button during download
-						} else if (rows.get(transferId).isDownload) {
+						} else if (rows.get(transferId).getTransferType() == Transfer.DOWNLOAD) {
 							downloadOperations.terminateTransfer(transferId);
 							rows.get(transferId).setCanceled(true);
 							((UpdatableTableModel) table.getModel())
@@ -312,7 +330,7 @@ public class TransferProgressTable {
 											true);
 							button.setText("Details");
 							// user clicks the "Cancel" button during upload
-						} else {
+						} else if (rows.get(transferId).getTransferType() == Transfer.UPLOAD) {
 							uploadOperations.terminateTransfer(transferId);
 							rows.get(transferId).setCanceled(true);
 							((UpdatableTableModel) table.getModel())
@@ -320,6 +338,14 @@ public class TransferProgressTable {
 											true);
 							button.setText("Details");
 
+						} else if (rows.get(transferId).getTransferType() == Transfer.DELETE) {
+							// TODO
+							deleteOperations.terminateTransfer(transferId);
+							rows.get(transferId).setCanceled(true);
+							((UpdatableTableModel) table.getModel())
+									.updateTransferStatus(transferId, 0, null,
+											true);
+							button.setText("Details");
 						}
 						fireEditingStopped();
 					} catch (FileTransferException e1) {
@@ -350,7 +376,7 @@ public class TransferProgressTable {
 	// data per table row
 	class RowData {
 		private boolean isCanceled;
-		private boolean isDownload;
+		private int transferType;
 		private String source;
 		private String destination;
 		private String currentFile;
@@ -360,12 +386,13 @@ public class TransferProgressTable {
 		private Date startTransferDate;
 		private Date endTransferDate;
 		private int transferId;
+		private String error = "N/A";
 
-		public RowData(String source, String destination, boolean isDownload, int transferId) {
+		public RowData(String source, String destination, int transferType, int transferId) {
 			this.source = source;
 			this.destination = destination;
 			this.progress = 0f;
-			this.isDownload = isDownload;
+			this.transferType = transferType;
 			this.actionBtnLabel = "Cancel";
 			this.transferId = transferId;
 		}
@@ -392,14 +419,6 @@ public class TransferProgressTable {
 
 		public void setProgress(float progress) {
 			this.progress = progress;
-		}
-
-		public boolean isDownload() {
-			return isDownload;
-		}
-
-		public void setDownload(boolean isDownload) {
-			this.isDownload = isDownload;
 		}
 
 		public String getCurrentFile() {
@@ -456,6 +475,22 @@ public class TransferProgressTable {
 
 		public void setTransferId(int transferId) {
 			this.transferId = transferId;
+		}
+
+		public int getTransferType() {
+			return transferType;
+		}
+
+		public void setTransferType(int transferType) {
+			this.transferType = transferType;
+		}
+
+		public String getError() {
+			return error;
+		}
+
+		public void setError(String error) {
+			this.error = error;
 		}
 	}
 
@@ -557,8 +592,8 @@ public class TransferProgressTable {
 			}
 		}
 
-		public int addTransfer(String source, String dest, boolean isDownload) {
-			RowData rowData = new RowData(source, dest, isDownload, rows.size());
+		public int addTransfer(String source, String dest, int transferType) {
+			RowData rowData = new RowData(source, dest, transferType, rows.size());
 			rowData.setQueuedDate(new Date());
 			rows.add(rowData);
 			fireTableRowsInserted(rows.size() - 1, rows.size() - 1);
@@ -585,6 +620,12 @@ public class TransferProgressTable {
 				/*
 				 * trigger the above trick
 				 */
+				if (rowData.transferType == Transfer.DELETE) {
+					if (progress == 2) 
+						rowData.setError("Cloud Error: File was not deleted \n");
+					else if (progress == 3)
+						rowData.setError("Cloud Exception \n");
+				}
 				rowData.setCanceled(true);
 				rowData.setEndTransferDate(new Date());
 				setValueAt((int) (rowData.getProgress() * 100), rowId, 3);

MyCloudJ_/src/cloud_interfaces/CloudCapabilities.java
@@ -1,0 +1,8 @@
+package cloud_interfaces;
+
+public interface CloudCapabilities {
+	boolean isDownloadSupported();
+	boolean isUploadSupported();
+	boolean isRenameSUpported();
+	boolean isDeleteSupported();
+}

MyCloudJ_/src/cloud_interfaces/CloudOperations.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface CloudOperations {
 	public void login() throws CloudException;
 	
+	public CloudCapabilities getCloudCapabilities();
+	
 	public void disconnect() throws CloudException;
 
 	public void downloadFile(String cloudPath, String localPath, CloudTransferCallback callback)
@@ -18,6 +20,8 @@ public interface CloudOperations {
 
 	public void uploadFolder(String localPath, String cloudPath, CloudTransferCallback callback)
 			throws CloudException;
+	
+	public boolean deleteFile(String cloudPath) throws CloudException;
 
 	public boolean isFile(String name) throws CloudException;
 	

MyCloudJ_/src/dropbox_backend/DropboxCapabilities.java
@@ -1,0 +1,27 @@
+package dropbox_backend;
+
+import cloud_interfaces.CloudCapabilities;
+
+public class DropboxCapabilities implements CloudCapabilities{
+
+	@Override
+	public boolean isDownloadSupported() {
+		return true;
+	}
+
+	@Override
+	public boolean isUploadSupported() {
+		return true;
+	}
+
+	@Override
+	public boolean isRenameSUpported() {
+		return false;
+	}
+
+	@Override
+	public boolean isDeleteSupported() {
+		return false;
+	}
+
+}

MyCloudJ_/src/dropbox_backend/DropboxOperations.java
@@ -1,5 +1,6 @@
 package dropbox_backend;
 
+import cloud_interfaces.CloudCapabilities;
 import cloud_interfaces.CloudException;
 import cloud_interfaces.CloudFile;
 import cloud_interfaces.CloudOperations;
@@ -48,6 +49,11 @@ public class DropboxOperations implements CloudOperations {
 
 		GeneralUtility.openDefaultBrowser(authorizeUrl);
 	}
+	
+	@Override
+	public CloudCapabilities getCloudCapabilities() {
+		return new DropboxCapabilities();
+	}
 
 	// Function to accept the access code and link the account of the user
 	public void DbxLinkUser(String authCode) throws CloudException {
@@ -76,7 +82,7 @@ public class DropboxOperations implements CloudOperations {
 		}
 
 	}
-
+	
 	@Override
 	public void disconnect() throws CloudException {
 		// TODO: close resources
@@ -314,6 +320,12 @@ public class DropboxOperations implements CloudOperations {
 		}
 
 		return fileList;
+	}
+	
+	@Override
+	public boolean deleteFile(String cloudPath) throws CloudException {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	private String formatPathForDbx(String path) {

commit b1a7efb454dca7051be4455f6401e732792f0d6e

Fix: Pack the browsing frame after file delete
Fixes: 55172201d07bf9e5d66b551986bb5183544bf450

MyCloudJ_/src/CloudGui/CloudFileTree.java
@@ -525,12 +525,16 @@ public class CloudFileTree {
 		@Override
 		public void updateGUI() {
 			DefaultMutableTreeNode downloadNode = getNodeFromPath(downloadRoot, selectedNodePath);
-			if (downloadNode != null)
+			if (downloadNode != null) {
 				downloadTreeModel.removeNodeFromParent(downloadNode);
+				getEnclosingFrame().pack();
+			}
 			
 			DefaultMutableTreeNode uploadNode = getNodeFromPath(uploadRoot, selectedNodePath);
-			if (uploadNode != null)
+			if (uploadNode != null) {
 				uploadTreeModel.removeNodeFromParent(uploadNode);
+				getEnclosingFrame().pack();
+			}
 		}
 	}
 }